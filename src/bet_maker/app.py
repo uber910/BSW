8045a5fb44584e8ec33ba6687d4d79f900ca0d82
Dima <dzmitry.dankov@interexy.com>
2026-05-18 02:15:50 +0200

    feat(05-07): wire rabbit_router into bet-maker app via include_router

--- src/bet_maker/app.py
+++ src/bet_maker/app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from bet_maker.entrypoints.api import bets, events, health
 from bet_maker.entrypoints.lifespan import lifespan
+from bet_maker.entrypoints.messaging import router as rabbit_router
 from bet_maker.entrypoints.middleware import RequestContextMiddleware
 
 
@@ -17,4 +18,5 @@ def build_app() -> FastAPI:
     app.include_router(health.router)
     app.include_router(bets.router)
     app.include_router(events.router)
+    app.include_router(rabbit_router)
     return app
